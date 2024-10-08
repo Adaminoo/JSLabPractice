9f4648a116009caa0b0ded824c3579cff7388dbe
Adam Brunner <adamino@Adams-MacBook-Pro.local>
2024-10-08 09:11:01 -0600

    added addbid functions to bidding app, save/load funcs are still buggy

--- biddingapp/main.js
+++ biddingapp/main.js
@@ -14,3 +14,17 @@ function addBid2() {
     array2.push("<li>" + num2 + "</li>");
     document.getElementById("array2").innerHTML = array2.join("");
 }
+
+function saveBids() {
+    localStorage.setItem("savedbids1", array1)
+    localStorage.setItem("savedbids2", array2)
+}
+
+function loadBids() {
+    let reload1 = []
+    reload1 = localStorage.getItem("savedbids1")
+    array1 = reload1
+    console.log(array1)
+    console.log(reload1)
+    document.getElementById("array1").innerHTML = array1.join("");
+}
